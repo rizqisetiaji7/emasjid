a87c2a7a2659e0b8c4ffe00530edce3ddf950089
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-05-17 18:17:49 +0700

    Add nullable to id_masjid foreign key

--- database/migrations/2023_05_15_111039_add_masjid_id_to_users_table.php
+++ database/migrations/2023_05_15_111039_add_masjid_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('masjid_id')->after('id')->index();
+            $table->foreignId('masjid_id')->after('id')->index()->nullable();
         });
     }
 
