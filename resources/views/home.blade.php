7d140ecbf8e04fba4fa8d8f507d93670a836b83f
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-05-18 21:21:40 +0700

    Change layout in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.main')
+
+@section('title', 'Home')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="row">
+	<h4 class="h4 mb-3">Welcome, <span class="fw-bold">{{ auth()->user()->name }}</span>.</h4>
+	
+	<div class="col-12">
+		<div class="card">
+			<div class="card-header">
+				<h5 class="card-title mb-0">Card Header</h5>
+			</div>
+			<div class="card-body">
+				{{-- Content --}}
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
+
+@push('script')
+<script>
+	
+</script>
+@endpush
