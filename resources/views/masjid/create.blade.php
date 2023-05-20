b05236cb41b80bbb3887d9d9bb92427961ff1b3a
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-05-21 02:17:42 +0700

    Create create masjid form in view blade

---
+++ resources/views/masjid/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'Informasi Masjid')
+
+@section('content')
+   <div class="row">
+      <div class="col-12">
+         <div class="card">
+            <div class="card-body">
+               {{-- Content Form --}}
+            </div>
+         </div>
+      </div>
+   </div>
+@endsection
