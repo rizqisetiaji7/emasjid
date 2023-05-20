58f9163cf686dcd7f8836598b4e8b7f537a234e7
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-05-21 02:16:48 +0700

    Update asset profile link image

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,7 @@
                <i class="align-middle" data-feather="settings"></i>
             </a>
             <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-               <img src="./dist/img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="{{ auth()->user()->name }}"> 
+               <img src="{{ asset('/dist/img/avatars/avatar.jpg') }}" class="avatar img-fluid rounded me-1" alt="{{ auth()->user()->name }}"> 
                <span class="text-dark">{{ auth()->user()->name }}</span>
             </a>
             <div class="dropdown-menu dropdown-menu-end">
