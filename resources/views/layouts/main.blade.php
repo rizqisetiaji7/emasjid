93bd913af225e484368bdcf5159c500ddd6955af
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-05-21 01:38:55 +0700

    Add bootstrap CDN

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,8 @@
    <meta name="csrf-token" content="{{ csrf_token() }}">
 
    <link rel="shortcut icon" href="{{ asset('/dist/img/icons/icon-48x48.png') }}" />
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
    <link rel="stylesheet" href="{{ asset('/dist/css/app.css') }}">
 	
    <title>@yield('title') | {{ config('app.name', 'E-Masjid') }}</title>
@@ -40,6 +42,7 @@
 		</div>
 	</div>
 
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 	<script src="{{ asset('/dist/js/app.js') }}"></script>
 	@stack('script')
 </body>
