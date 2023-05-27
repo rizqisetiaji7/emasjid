73ca091903498e7d74e8cb4f2d67f0eff6aea842
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-05-27 10:21:40 +0700

    Store mosque data to the storage

--- app/Http/Controllers/MasjidController.php
+++ app/Http/Controllers/MasjidController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Masjid;
 use App\Http\Requests\StoreMasjidRequest;
 use App\Http\Requests\UpdateMasjidRequest;
+use App\Models\User;
 
 class MasjidController extends Controller
 {
@@ -21,7 +22,7 @@ class MasjidController extends Controller
      */
     public function create()
     {
-        $masjid = new Masjid();
+        $masjid = auth()->user()->masjid ?? new Masjid();
         return view('masjid.create', compact('masjid'));
     }
 
@@ -30,7 +31,24 @@ class MasjidController extends Controller
      */
     public function store(StoreMasjidRequest $request)
     {
-        //
+        // Check the mosque management data
+        $masjid = auth()->user()->masjid ?? new Masjid();
+
+        // Set and save mosque data values ​​from user requests
+        $masjid->nama    = $request->nama;
+        $masjid->telepon = $request->telepon;
+        $masjid->email   = $request->email;
+        $masjid->alamat  = $request->alamat;
+        $masjid->save();
+
+        // Update the mosque id on the user as the administrator of the mosque
+        $user = User::where('id', auth()->user()->id)->first();
+        $user->masjid_id = $masjid->id;
+        $user->update();
+
+        // Send a flash message and return to the mosque form page
+        flash('Data masjid telah berhasil disimpan.')->success();
+        return redirect()->back();
     }
 
     /**
